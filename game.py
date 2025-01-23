b8ce132ca6c52bdf72f2ec5901843ab300d99e95
Sammy <sameerpanday@mail.weber.edu>
2025-01-23 10:10:50 -0700

    SammyNG

--- game.py
+++ game.py
@@ -1,2 +1,6 @@
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
+# Ask for the player's name
+player_name = input("What is your name, ?")
+# Concenate strings to create a personalized message
+print("Welcome, " + player_name +"! Your Journey Begins now.")
